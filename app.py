c97fff0ea715cc1bd6b95d8929c0472c2c4dc973
PandiBose <111458502+PandiBose@users.noreply.github.com>
2024-04-26 12:24:53 +0000

    upto render template

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask,render_template
+from flask import Flask, jsonify,render_template
 
 app = Flask(__name__)
+
+JOBS=[
+  {
+    'id1':1,
+    'title':'Data Analyst',
+    'location':'Bengaluru',
+    'salary':'Rs. 10,00,000'
+  },
+  {
+    'id1':2,
+    'title':'Data Engineer',
+    'location':'Bengaluru',
+    'salary':'Rs. 10,00,000'
+  },
+  {
+    'id1':3,
+    'title':'Data Scientist',
+    'location':'Bengaluru',
+    'salary':'Rs. 10,00,000'
+  }
+]
 
 
 @app.route('/')
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",jobs=JOBS,name="Arun")
+
+@app.route('/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
